d2ef5f90bb2ed682b61e74416fe42551ec10b1d8
Vivek Chandel <63957335+MRVIVEK-CODER@users.noreply.github.com>
2021-05-16 08:29:10 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+clear
+echo
+pkg install python
+clear
+pkg install figlet
+clear
+pkg install ruby -y
+clear
+gem install lolcat
+clear
+figlet VivekXD | lolcat
+sleep 2
+clear
+figlet VivekXD | lolcat
+sleep 2
+clear
+echo
+echo "██    ██ ██   ██ ███    ██ ███████ " | lolcat
+echo " ██  ██  ██   ██ ████   ██    ███  " | lolcat
+echo "  ████   ███████ ██ ██  ██   ███   " | lolcat
+echo "   ██         ██ ██  ██ ██  ███    " | lolcat
+echo "   ██         ██ ██   ████ ███████ " | lolcat
+echo
+clear
+echo
+echo "╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮" | lolcat
+echo "╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯" | lolcat
+echo "┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈" | lolcat
+echo "┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈" | lolcat
+echo "╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮" | lolcat
+echo "╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯" | lolcat
+echo
+clear
+pip2 install -r requirements.txt
+clear
+pip2 install mechanize
+clear
+pip2 install requests
+clear
+python2 mbf1.py
